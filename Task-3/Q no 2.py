aa2942ad50fd73329bf9af1559ccb2af96463657
dilip-kumar-g <107037057+dilip-kumar-g@users.noreply.github.com>
2024-01-16 20:10:21 +0530

    Update Q no 2.py

--- Task-3/Q no 2.py
+++ Task-3/Q no 2.py
@@ -1,8 +1,5 @@
 
 # Given a list of numbers, return the no of prime numbers in it and list them 
 
-prime_numbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
-
-odd = [3,5,7,9]
 
 
