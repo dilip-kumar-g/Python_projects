eff00b1966ae9969e4bd2748c3c5e78a42ed95fd
dilip-kumar-g <107037057+dilip-kumar-g@users.noreply.github.com>
2024-01-19 18:48:56 +0530

    Update Q no 2.py

--- Task-3/Q no 2.py
+++ Task-3/Q no 2.py
@@ -1,5 +1,15 @@
 
 # Given a list of numbers, return the no of prime numbers in it and list them 
 
+numbers = [10, 501, 23, 22, 37, 100, 999, 87, 351]
 
+for num in numbers:
+    y = int((num / 2) + 1)
+
+    for i in range(2, y):
+        if num % i == 0:
+            print(num, "is not a prime number")
+            break
+    else:
+        print(num, "is a prime number")
 
